b9d8091289025d3716cbeb2ef744e3f08a423c8d
Denis Tauchman <denis.tauchman@biano.com>
2022-12-04 19:42:25 +0100

    lmao py

--- 4/4_2.py
+++ 4/4_2.py
@@ -2,9 +2,7 @@ import io
 def main(file=[]):
     r = 0
     for line in file:
-        x, y = line.split(",")
-        x1, x2 = map(int, x.split("-"))
-        y1, y2 = map(int, y.split("-"))
+        (x1, x2), (y1, y2) = map(lambda x: map(int, x.split("-")), line.split(","))
         x = list(range(x1, x2+1))
         y = list(range(y1, y2+1))
         l = lambda a, b: a in b
