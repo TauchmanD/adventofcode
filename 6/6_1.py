fd9e180c38280efa7589787cbaba521298b7ac80
Denis Tauchman <denis.tauchman@biano.com>
2022-12-06 19:36:06 +0100

    day 6

---
+++ 6/6_1.py
@@ -0,0 +1,20 @@
+import io
+
+
+def main(file=[]):
+    buffer_size = 4
+    r = buffer_size
+    buffer = list(file[:buffer_size])
+    for c in file[buffer_size:]:
+        if len(set(buffer)) == buffer_size:
+            break
+        buffer.append(c)
+        buffer.pop(0)
+        r+=1
+    print(r)
+
+
+if __name__ == "__main__":
+    with io.open("input.txt", "r") as f:
+        file = f.read()
+    main(file)
